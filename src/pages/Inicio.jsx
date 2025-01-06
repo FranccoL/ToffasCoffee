339b54b0e8e6523fa9138bd4e73a98d0acf87b90
FranccoL <leofranco96@gmail.com>
2025-01-06 16:13:22 -0300

    repair path on page

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header/Header"
 import Hero from "../components/Hero/Hero"
 import PqNos from "../components/PqNos/PqNos"
 import ComoFunciona from "../components/ComoFunciona/ComoFunciona"
-import CardInfo from "../components/CardInfo/cardInfo"
+import CardInfo from "../components/CardInfo/CardInfo"
 import SectionAboutToffa from "../components/SectionAboutToffa/SectionAboutToffa"
 import Footer from "../components/Footer/Footer"
 
