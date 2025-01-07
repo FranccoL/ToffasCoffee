7ad1330d2088553ae3ab4d4cd9e26db71345fa4a
FranccoL <leofranco96@gmail.com>
2025-01-07 10:37:41 -0300

    add version mobile

--- src/components/ComoFunciona/ComoFunciona.jsx
+++ src/components/ComoFunciona/ComoFunciona.jsx
@@ -1,36 +1,34 @@
-import './ComoFunciona.css'
+import './ComoFunciona.css';
 
 function ComoFunciona () {
-    return (
-      <div className="container">
-        <div className="tittleHow">
-        <h1>COMO FUNCIONA? </h1>
+  return (
+    <div className="container">
+      <div className="tittleHow">
+        <h1>COMO FUNCIONA?</h1>
+      </div>
+
+      <div className="d-flex image-coffe">
+        <div className="whiteCard">
+          <div className="iconsHow">
+            <div className="iconCoffe"></div>
+            <h1 className="tittleIcon">ESCOLHA SEU CAFÉ</h1>
+          </div>
+          <div className="iconsHow">
+            <div className="iconWhatsapp"></div>
+            <h1 className="tittleIcon">ENTRE EM CONTATO</h1>
+          </div>
+          <div className="iconsHow">
+            <div className="iconDelivery"></div>
+            <h1 className="tittleIcon">APROVEITE A SUA ENTREGA</h1>
+          </div>
+
+          <div className="buttonHowM">
+            <button className="buttonHow" type="submit">Entre em Contato</button>
+          </div>
         </div>
-
-        <div className=" d-flex image-coffe">
-            <div className="whiteCard d-flex">
-            </div>
-                <div className="iconsHow d-flex jc-space-evenlyn ">
-                    <div className="iconCoffe"></div>
-                    <div className="iconWhatsapp"></div>
-                    <div className="iconDelivery"></div>
-                </div>
-
-            <div className="tittleIcons d-flex jc-space-evenlyn al-center">
-                <h1 className="tittleIcon1">ESCOLHA SEU CAFÉ</h1>
-                <h1 className="tittleIcon2">ENTRE EM CONTATO</h1>
-                <h1 className="tittleIcon3">APROVEITE A SUA ENTREGA</h1>
-            </div>
-            <div className="buttonHowM">
-                <button className="buttonHow al-center" type='submit'> Entre em Contato </button>     
-            </div>
-        </div>
-</div>
-
-           
-
-
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ComoFunciona
+export default ComoFunciona;
